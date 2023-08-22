3eaa06d1852248d89f2cad22839c688e47f5a171
Manuel Pebay <manupebay@hotmail.com>
2023-08-22 16:14:11 -0300

    Sitio Final Completo

--- assets/js/detalles.js
+++ assets/js/detalles.js
@@ -19,45 +19,18 @@ fetch("https://mindhub-xj03.onrender.com/api/petshop")
 })
 
 function crearTarjeta(objeto){
-    return `<section class="card shadow mb-5 bg-body-tertiary rounded" style="width: 18rem;">
-                <img src="${objeto.imagen}" class="card-img-top" alt="school_book_fair">
-                <div class="card-body d-flex flex-column align-items-center text-center">
-                    <h5 class="card-title">${objeto.producto}</h5>
-                    <p class="card-text">${objeto.descripcion}</p>
-                    <p>Categoria: ${objeto.categoria}</p>
-                </div>
-                <div class="card-body d-flex flex-column justify-content-around align-items-center">
-                    <p>Precio: $${objeto.precio}</p>
-                    <p>Disponibles: ${objeto.disponibles}</p>
-                </div>
-            </section>`
+    return `<section class="shadow mb-4 bg-body-tertiary rounded col-12 col-md-6 col-lg-6">
+    <img src="${objeto.imagen}" class="imgCard p-2 object-fit-contain card-img-top" alt="school_book_fair">
+    <div class="card-body text-center">
+        <h5 class="card-title">${objeto.producto}</h5>
+        <p class="card-text">${objeto.descripcion}</p>
+        <p>Categoria: ${objeto.categoria}</p>
+        <p>Precio: $${objeto.precio}</p>
+        <p>Disponibles: ${objeto.disponibles}</p>
+    </div>
+</section>`
 }
 
 function renderizarTarjeta(contenedorHTML, string){
     contenedorHTML.innerHTML = string
 }
-
-/*
-
-function crearTarjeta(objeto){
-    return `<section class="card" style="width: 18rem;">
-                <img src="${objeto.image}" class="card-img-top" alt="school_book_fair">
-                <div class="card-body d-flex flex-column align-items-center text-center">
-                    <h5 class="card-title">${objeto.name}</h5>
-                    <p class="card-text">${objeto.description}</p>
-                    <p>${objeto.category}</p>
-                </div>
-                <div class="card-body d-flex flex-column justify-content-around align-items-center">
-                    <p>Price: $${objeto.price}</p>
-                    <p>Date: ${objeto.date}</p>
-                    <p>Location: ${objeto.place}</p>
-                    <p>${objeto.assistance?(`Assistance: ${objeto.assistance}`):(`Estimate: ${objeto.estimate}`)}</p>
-                    <p>Capacity: ${objeto.capacity}</p>
-                </div>
-            </section>`
-}
-
-function renderizarTarjeta(contenedorHTML, string){
-    contenedorHTML.innerHTML = string
-}
-*/
